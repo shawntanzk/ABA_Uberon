21ae82c8f054b0a45453ab94270b1a9762acbbc0
hkir-dev <huseyin_kir@yahoo.com>
2022-03-08 12:47:37 +0000

    Allen JSON structuregraph to OWL converter #9

--- src/scripts/structure_graph_template.py
+++ src/scripts/structure_graph_template.py
@@ -2,36 +2,42 @@ import argparse
 import json
 import pandas as pd
 import csv
+import ntpath
 
-OBO_MBA = "http://purl.obolibrary.org/obo/MBA_"
+
+NAMESPACES = {"1.json": "http://purl.obolibrary.org/obo/MBA_",
+              "17.json": "http://purl.obolibrary.org/obo/DMBA_",
+              "10.json": "http://purl.obolibrary.org/obo/HBA_",
+              "16.json": "http://purl.obolibrary.org/obo/DHBA_",
+              "8.json": "http://purl.obolibrary.org/obo/PHA_"}
 
 
 def generate_template(graph_json, output):
     f = open(graph_json, 'r')
     j = json.loads(f.read())
     data_list = list()
+    namespace = NAMESPACES[ntpath.basename(graph_json)]
 
     for root in j["msg"]:
-        tree_recurse(root, data_list)
+        tree_recurse(root, data_list, namespace)
 
     data_template = pd.DataFrame.from_records(data_list)
     data_template.to_csv(output, sep="\t", index=False, quoting=csv.QUOTE_NONE)
     f.close()
 
 
-def tree_recurse(node, dl):
+def tree_recurse(node, dl, namespace):
     d = dict()
-    d["id"] = OBO_MBA + str(node["id"])
-    # print(d["id"])
+    d["id"] = namespace + str(node["id"])
     d["name"] = str(node["name"])
     d["acronym"] = node["acronym"]
     if node["parent_structure_id"]:
-        d["parent_structure_id"] = OBO_MBA + str(node["parent_structure_id"])
+        d["parent_structure_id"] = namespace + str(node["parent_structure_id"])
     d["subclass_of"] = "UBERON:0002616"
     dl.append(d)
 
     for child in node["children"]:
-        tree_recurse(child, dl)
+        tree_recurse(child, dl, namespace)
 
 
 parser = argparse.ArgumentParser(description='Cli interface structure graph linkml template generation.')

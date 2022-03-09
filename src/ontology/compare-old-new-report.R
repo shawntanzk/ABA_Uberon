289fd8717745669de293ad44dac9e38e701f746d
Shawn Tan <76212760+shawntanzk@users.noreply.github.com>
2022-03-09 09:19:25 +0000

    remove convert

---
+++ src/ontology/compare-old-new-report.R
@@ -0,0 +1,13 @@
+# Import reports
+old_report <- read.table(file = '/Users/shawntan/documents/GitHub/ABA_Uberon/src/ontology/old-report.tsv')
+new_report <- read.table(file = '/Users/shawntan/documents/GitHub/ABA_Uberon/src/ontology/report.tsv')
+
+install.packages("sqldf")
+
+require(sqldf)
+
+diff_old_new <- sqldf('SELECT * FROM old_report EXCEPT SELECT * FROM new_report')
+diff_new_old <- sqldf('SELECT * FROM new_report EXCEPT SELECT * FROM old_report')
+
+write.table(diff_old_new, file='/Users/shawntan/documents/GitHub/ABA_Uberon/src/ontology/report/old-to-new.tsv', quote=FALSE, sep='\t')
+write.table(diff_new_old, file='/Users/shawntan/documents/GitHub/ABA_Uberon/src/ontology/report/new-to-old.tsv', quote=FALSE, sep='\t')

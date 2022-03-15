ad300a0c3f22756181b60ad968aa8acaff323dbd
hkir-dev <huseyin_kir@yahoo.com>
2022-03-15 12:22:01 +0000

    linkout template fixed based on the new ont

--- src/scripts/gen_linkout_template.py
+++ src/scripts/gen_linkout_template.py
@@ -27,19 +27,17 @@ tab = [seed]
 for n in g['nodes']:
     if 'type' in n.keys() and n['type'] == 'CLASS':
         lstat = True
-        if 'meta' in n.keys() and 'basicPropertyValues' in n['meta'].keys():
-            for bpv in n['meta']['basicPropertyValues']:
-                for k, v in mapping.items():
-                    if bpv['pred'] == 'http://purl.obolibrary.org/obo/%s#identifier' % k:
-                        for a in v['atlases']:
-                            tab.append({'ID': n['id'],
-                                        'xref': link.substitute(atlas_id=a['id'],
-                                                                structure_id=bpv['val']),
-                                        'prefLabel': ' '.join([n['lbl'],
-                                                               ' (',
-                                                               v['species'],
-                                                               ')'])})
-                        lstat = False
+        for k, v in mapping.items():
+            if str(n['id']).lower().startswith('http://purl.obolibrary.org/obo/%s' % k):
+                for a in v['atlases']:
+                    tab.append({'ID': n['id'],
+                                'xref': link.substitute(atlas_id=a['id'],
+                                                        structure_id=str(n['id']).rsplit('_', 1)[-1]),
+                                'prefLabel': ' '.join([n['lbl'],
+                                                       ' (',
+                                                       v['species'],
+                                                       ')'])})
+                lstat = False
         if lstat and 'lbl' in n.keys():
             tab.append({'ID': n['id'], 'xref': '', 'prefLabel': n['lbl']})
 
